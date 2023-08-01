f83de661190db5e041d56cee8708c229d2eee23f
Brian Greene <brian.greene@hmdlabs.io>
2023-08-01 10:39:00 -0500

    feat:  a roughly working velocity template to produce the intermediate json

--- src/pyscratch/local_nw_extract.sh
+++ src/pyscratch/local_nw_extract.sh
@@ -1,5 +1,11 @@
-schemacrawler.sh --command=serialize --host=localhost --port=5432 --user=postgres --password=admin --server=postgresql --database=northwind --info-level=standard --output-format=json > ../../test/stuff.json
-schemacrawler.sh --command=serialize --host=localhost --port=5432 --user=postgres --password=admin --server=postgresql --database=northwind --info-level=standard --output-format=yaml > ../../test/stuff.yaml
-#schemacrawler.sh --command=template --host=localhost --port=5432 --user=postgres --password=admin --server=postgresql --database=northwind --info-level=standard --log-level=config --templating-language=velocity --template "./tables"
+#schemacrawler.sh --command=serialize --host=localhost --port=5432 --user=postgres --password=admin --server=postgresql --database=northwind --info-level=standard --output-format=json > ../../test/stuff.json
+schemacrawler.sh --command=template --host=localhost --port=5432 --user=postgres --password=admin --server=postgresql --database=northwind --info-level=standard --templating-language=velocity --template "intermediate_json.vm" > inter.json
+
+#schemacrawler.sh --command=schema --host=localhost --port=5432 --user=postgres --password=admin --server=postgresql --database=northwind --info-level=standard --output-format=html
+
+#schemacrawler.sh -h
+
+#schemacrawler.sh --command=script --host=localhost --port=5432 --user=postgres --password=admin --server=postgresql --database=northwind --info-level=standard --script=sc_plantuml.py > sc_puml.puml
+
 
 #schemacrawler.sh --command=schema --host=localhost --port=5432 --user=postgres --password=admin --server=postgresql --database=northwind --info-level=standard -F html > report.html
